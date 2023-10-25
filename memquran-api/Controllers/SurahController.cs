a3fd47085374417e6fe93957b604c144497337a6
Atif Rehman <atif.rehman@outlook.com>
2023-10-25 21:04:15 +0100

    Adding surah controller

---
+++ memquran-api/Controllers/SurahController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuranApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SurahController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var surahsText = await System.IO.File.ReadAllTextAsync("Resources/Surahs/surahs.json");
+        
+        return Ok(surahsText);
+    }
+}
