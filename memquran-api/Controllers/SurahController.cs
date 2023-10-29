e4c07fd28e5cf6895b62a0b16c34b4f25d48ee04
Atif Rehman <atif.rehman@outlook.com>
2023-10-29 21:20:28 +0000

    Add logging the time to get surahs.json

--- memquran-api/Controllers/SurahController.cs
+++ memquran-api/Controllers/SurahController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace QuranApi.Controllers;
 
@@ -6,10 +7,20 @@ namespace QuranApi.Controllers;
 [Route("[controller]")]
 public class SurahController : ControllerBase
 {
+    private readonly ILogger<SurahController> _logger;
+
+    public SurahController(ILogger<SurahController> logger)
+    {
+        _logger = logger;
+    }
+    
     [HttpGet("{languageCode}")]
     public async Task<IActionResult> Get([FromRoute] string languageCode)
     {
+        var sw = Stopwatch.StartNew();
         var surahsText = await System.IO.File.ReadAllTextAsync("Resources/Surahs/surahs.json");
+
+        _logger.LogInformation("Surahs text loaded in {Elapsed} ms", sw.Elapsed);
         
         return Ok(surahsText);
     }
