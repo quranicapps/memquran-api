03ea072c853391ea5fe12e62d28cbb7e2edda731
Atif Rehman <atif.rehman@outlook.com>
2023-11-09 20:58:34 +0000

    Adding Surah verse json files

--- memquran-api/Controllers/SurahController.cs
+++ memquran-api/Controllers/SurahController.cs
@@ -22,22 +22,19 @@ public class SurahController : ControllerBase
     public async Task<IActionResult> Get([FromRoute] string locale, string surahNumber)
     {
         var sw = Stopwatch.StartNew();
-        
-        // await Task.Delay(1000); 
-        
-        _logger.LogInformation("Surah - {Locale}:{SurahNumber} loaded in {Elapsed} ms", locale, surahNumber, sw.Elapsed);
-    
-        return Ok(new SurahModel { SurahInfo = new SurahInfo { Chapter = int.Parse(surahNumber) } });
-    }
-    
-    public class SurahModel
-    {
-        public SurahInfo SurahInfo { get; set; }
-    }
 
-    public class SurahInfo
-    {
-        [JsonPropertyName("Chapter")]
-        public int Chapter { get; set; }
+        var surahsText = await _cache.GetStringAsync($"{locale}_surah_{surahNumber}");
+        
+        if (surahsText is null)
+        {
+            _logger.LogInformation("Cache miss for {Locale}_surah_{SurahNumber}", locale, surahNumber);
+            using var streamReader = System.IO.File.OpenText($"Resources/surahs/{locale}/{locale}_surah_{surahNumber}.json");
+            surahsText = await streamReader.ReadToEndAsync();
+            await _cache.SetStringAsync($"{locale}_surah_{surahNumber}", surahsText);
+        }
+
+        _logger.LogInformation("Surah {Locale}_surah_{SurahNumber} text loaded in {Elapsed} ms", locale, surahNumber, sw.Elapsed);
+        
+        return Ok(surahsText);
     }
 }
--- memquran-api/Workers/CachingWorker.cs
+++ memquran-api/Workers/CachingWorker.cs
@@ -25,7 +25,18 @@ public class CachingWorker : BackgroundService
             var surahsBytes = await File.ReadAllBytesAsync(surahInfoFile, stoppingToken);
             var cacheKey = Path.GetFileNameWithoutExtension(surahInfoFile);
             await _cache.SetAsync($"{cacheKey}", surahsBytes, token: stoppingToken);
+        }
+        _logger.LogInformation("{Name} - Cached: {{locale}}_surahInfos", nameof(CachingWorker));
+
+        
+        // Cache Surah files
+        foreach (var surahInfoFile in Directory.GetFiles("Resources/surahs", "*.json", SearchOption.AllDirectories))
+        {
+            var surahsBytes = await File.ReadAllBytesAsync(surahInfoFile, stoppingToken);
+            var cacheKey = Path.GetFileNameWithoutExtension(surahInfoFile);
+            await _cache.SetAsync($"{cacheKey}", surahsBytes, token: stoppingToken);
             _logger.LogInformation("{Name} - Cached: {CacheKey}", nameof(CachingWorker), cacheKey);
         }
+        _logger.LogInformation("{Name} - Cached: {{locale}}_surahs", nameof(CachingWorker));
     }
 }
