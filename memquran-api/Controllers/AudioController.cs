a0a054ab6003f213d77f7705bf89a99d99221723
Atif Rehman <atif.rehman@outlook.com>
2023-12-18 15:59:14 +0000

    Add GetAudioJson

---
+++ memquran-api/Controllers/AudioController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using QuranApi.Contracts;
+
+namespace QuranApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AudioController : ControllerBase
+{
+    private readonly IStaticFileService _staticFileService;
+    private readonly ILogger<AudioController> _logger;
+
+    public AudioController(IStaticFileService staticFileService, ILogger<AudioController> logger)
+    {
+        _staticFileService = staticFileService;
+        _logger = logger;
+    }
+    
+    // http://localhost:3000/json/audio/ab_abdallah-al-matroud-1/timings/surah/abdallah-al-matroud-1_timings_surah_1.json
+    [HttpGet("/json/audio/{reciterId}/timings/surah/{fileName}")]
+    public async Task<IActionResult> GetAudioJson([FromRoute] string reciterId, [FromRoute] string fileName)
+    {
+        var sw = Stopwatch.StartNew();
+        
+        var text = await _staticFileService.GetFileContentStringAsync($"json/audio/{reciterId}/timings/surah/{fileName}");
+        
+        if (text is null)
+        {
+            return NotFound();
+        }
+
+        _logger.LogInformation("/json/audio/{ReciterId}/timings/surah/{FileName} loaded in {Elapsed} ms", reciterId, fileName, sw.Elapsed);
+        
+        return Ok(text);
+    }
+}
