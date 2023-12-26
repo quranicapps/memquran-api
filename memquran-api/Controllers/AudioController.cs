a856ff3e5769894ac46b2901a9f11f3d5dfd9422
Atif Rehman <atif.rehman@outlook.com>
2023-12-26 23:14:01 +0000

    Adding GetJuzAudioJson endpoint

--- memquran-api/Controllers/AudioController.cs
+++ memquran-api/Controllers/AudioController.cs
@@ -19,7 +19,7 @@ public class AudioController : ControllerBase
     
     // http://localhost:3000/json/audio/abderahmane-eloosi-1/timings/surah/abderahmane-eloosi-1_timings_surah_1.json
     [HttpGet("/json/audio/{reciterId}/timings/surah/{fileName}")]
-    public async Task<IActionResult> GetAudioJson([FromRoute] string reciterId, [FromRoute] string fileName)
+    public async Task<IActionResult> GetSurahAudioJson([FromRoute] string reciterId, [FromRoute] string fileName)
     {
         var sw = Stopwatch.StartNew();
         
@@ -31,6 +31,24 @@ public class AudioController : ControllerBase
         }
 
         _logger.LogInformation("/json/audio/{ReciterId}/timings/surah/{FileName} loaded in {Elapsed} ms", reciterId, fileName, sw.Elapsed);
+        
+        return Ok(text);
+    }
+    
+    // http://localhost:3000/json/audio/abu-bakr-ash-shaatree-1/timings/juz/abu-bakr-ash-shaatree-1_timings_juz_1.json
+    [HttpGet("/json/audio/{reciterId}/timings/juz/{fileName}")]
+    public async Task<IActionResult> GetJuzAudioJson([FromRoute] string reciterId, [FromRoute] string fileName)
+    {
+        var sw = Stopwatch.StartNew();
+        
+        var text = await _staticFileService.GetFileContentStringAsync($"json/audio/{reciterId}/timings/juz/{fileName}");
+        
+        if (text is null)
+        {
+            return NotFound();
+        }
+
+        _logger.LogInformation("/json/audio/{ReciterId}/timings/juz/{FileName} loaded in {Elapsed} ms", reciterId, fileName, sw.Elapsed);
         
         return Ok(text);
     }
